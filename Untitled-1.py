c1964729213a96770d3457f43092eb4e2b373fb2
MasonJarCat <104012434+MasonJarCat@users.noreply.github.com>
2022-11-21 13:18:26 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,2 @@
+first_name = "hello"
+print: first_name
---
+++ questgenerator.py
@@ -0,0 +1,7 @@
+#quest generator 
+#by: @Mason 
+#version: 0.1
+#date: 11/21/2022
+#description: this program will generate a quest for a player to complete
+
+
