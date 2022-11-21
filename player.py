72875706040d87641ccfa980d1efd895749a8cad
MasonJarCat <MasonMcClinticC@gmail.com>
2022-11-21 13:24:59 -0500

    yo

--- player.py
+++ player.py
@@ -1,2 +1,44 @@
-first_name = "hello"
-print: first_name
+import quest 
+class player:
+    def __init__(self, name, level, health, maxHealth, gold, inventory, xp,questlist):
+        self.name = name
+        self.level = level
+        self.health = health
+        self.maxHealth = maxHealth
+        self.gold = gold
+        self.inventory = inventory
+        self.xp = xp
+        self.questlist = questlist
+    def __str__(self):
+        return self.name + " " + str(self.level) + " " + str(self.health) + " " + str(self.maxHealth) + " " + str(self.gold) + " " + str(self.inventory)
+    def createPlayer(name, level, health, maxHealth, gold, inventory):
+        return player(name, level, health, maxHealth, gold, inventory)
+    def addxp(amount):
+        self.xp += amount
+    def removexp(amount):
+        self.xp -= amount
+    def addItem(item):
+        self.inventory.append(item)
+    def removeItem(item):
+        self.inventory.remove(item)
+    def addGold(amount):
+        self.gold += amount
+    def removeGold(amount):
+        self.gold -= amount
+    def addHealth(amount):
+        self.health += amount
+    def removeHealth(amount):
+        self.health -= amount
+    def addMaxHealth(amount):
+        self.maxHealth += amount
+    def removeMaxHealth(amount):
+        self.maxHealth -= amount
+    def addLevel(amount):
+        self.level += amount
+    def removeLevel(amount):
+        self.level -= amount
+    def add_questRewards(quest):
+        self.gold += quest.reward
+        self.xp += quest.xp
+    def add_quest(quest):
+        self.questlist.append(quest)
--- questgenerator.py
+++ questgenerator.py
@@ -3,5 +3,43 @@
 #version: 0.1
 #date: 11/21/2022
 #description: this program will generate a quest for a player to complete
+class quest:
+    questNames = ["drink water", "eat food" ]
+    questDescriptions = ["drink half of your water", "eat food at urban"]
+    questRewards = ["1000 gold", "500 gold"]
+    difficulty = ["easy", "medium", "hard"]
+    def __init__(self, name, description, reward, difficulty, xp):
+        self.name = name
+        self.description = description
+        self.reward = getReward(difficulty)
+        self.difficulty = difficulty
+        self.xp = getXP(difficulty)
 
-
+    def __str__(self):
+        return self.name + " " + self.description + " " + self.reward + " " + self.difficulty
+    def createRandomQuest():
+        name = random.choice(questNames)
+        description = random.choice(questDescriptions)
+        reward = random.choice(questRewards)
+        difficulty = random.choice(questDifficulties)
+        return quest(name, description, reward, difficulty)
+    def createQuest(name, description, reward, difficulty):
+        return quest(name, description, reward, difficulty)
+    def getReward(difficulty):
+        if difficulty == "easy":
+            return 1000
+        elif difficulty == "medium":
+            return 2000
+        elif difficulty == "hard":
+            return 3000
+        else:
+            return 0
+    def getXP(difficulty):
+        if difficulty == "easy":
+            return 100
+        elif difficulty == "medium":
+            return 200
+        elif difficulty == "hard":
+            return 300
+        else:
+            return 0
